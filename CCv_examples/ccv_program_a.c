d56b20b805db603f14c71a8f7488cc3d20574733
Omkar Tuppe <24736420+0Ccipher@users.noreply.github.com>
2021-07-30 12:39:34 +0530

    programs from fig1. ccv robustness paper

---
+++ CCv_examples/ccv_program_a.c
@@ -0,0 +1,48 @@
+#include <pthread.h>
+#include <assert.h>
+#include <stdio.h>
+
+int x=0,y=0,z=0;
+void __VERIFIER_atomic_t1(){	
+	z = 1;
+	x = 1;
+}
+
+void __VERIFIER_atomic_t2(){
+	y = 1;
+ }
+
+void __VERIFIER_atomic_t3(){
+	x = 2;
+	printf(" z3 = %d: ",z);
+}
+void __VERIFIER_atomic_t4(){
+	printf(" y4 = %d: ",y);
+	printf(" x4 = %d: ",x);
+}
+
+
+void *thr1(void *arg){
+	printf(" ");
+ 	__VERIFIER_atomic_t1();
+ 	__VERIFIER_atomic_t2();
+ 	
+	return NULL;
+}
+
+void *thr2(void *arg){
+	printf(" ");
+	 __VERIFIER_atomic_t3();
+	__VERIFIER_atomic_t4();
+	return NULL;
+
+}
+int main(int argc, char *argv[]){
+	pthread_t t1,t2;
+	pthread_create(&t1,NULL,thr1,NULL);
+	pthread_create(&t2,NULL,thr2,NULL);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	printf("\n");
+	return 0;
+}
---
+++ CCv_examples/ccv_program_c.c
@@ -0,0 +1,44 @@
+#include <pthread.h>
+#include <assert.h>
+#include <stdio.h>
+
+int x=0,y=0,z=0;
+void __VERIFIER_atomic_t1(){	
+	x = 2;
+}
+
+void __VERIFIER_atomic_t2(){
+ 	x = 1;
+ }
+
+void __VERIFIER_atomic_t3(){
+	printf(" x3 = %d: ",x);
+}
+void __VERIFIER_atomic_t4(){
+	printf(" x4 = %d: ",x);
+}
+
+
+void *thr1(void *arg){
+	printf(" ");
+ 	__VERIFIER_atomic_t1(); 	
+	return NULL;
+}
+
+void *thr2(void *arg){
+	printf(" ");
+	 __VERIFIER_atomic_t2();
+	__VERIFIER_atomic_t3();
+	__VERIFIER_atomic_t4();
+	return NULL;
+
+}
+int main(int argc, char *argv[]){
+	pthread_t t1,t2;
+	pthread_create(&t1,NULL,thr1,NULL);
+	pthread_create(&t2,NULL,thr2,NULL);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	printf("\n");
+	return 0;
+}
