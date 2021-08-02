4eca1102fd77fbcbf242e9f596b34f5950bba3f7
Omkar Tuppe <24736420+0Ccipher@users.noreply.github.com>
2021-08-02 15:45:06 +0530

    Add files via upload

--- CCv_examples/test2.c
+++ CCv_examples/test2.c
@@ -1,43 +1,36 @@
 #include <pthread.h>
 #include <assert.h>
 #include <stdio.h>
-#include <stdatomic.h>
 
-int x=0,y=0;
+int x=0,y=0,z=0;
 void __VERIFIER_atomic_t1(){	
 	y = 1;
 	x = 1;
-	printf("y1 = %d: ",y);
 }
 
 void __VERIFIER_atomic_t2(){
-	printf("x : %d", x);
+	printf("x2 : %d", x);
   x = 2;
  }
 
 void __VERIFIER_atomic_t3(){
-	y = 4;
-	y = 4;
 	y = 3;
 	x = 3;
 }
 void __VERIFIER_atomic_t4(){
 	//y = 10;
 	y = 4;
-	printf("x4 = %d: ",x);
-	//assert( y < 0);
+	printf(" x4 = %d: ",x);
 }
 
 void __VERIFIER_atomic_t5(){
-	y = 4;
-	y = 4;
-	y = 3;
-	//y = x;
-	//assert(x <= 0);
-	x = 3;
+	printf(" x5_2 := %d", x);
+	assert( x== 3);
+	printf(" z5 := %d", z);
 }
 
 void *thr1(void *arg){
+	printf(" ");
  	__VERIFIER_atomic_t1();
  	__VERIFIER_atomic_t2();
  	
@@ -45,6 +38,7 @@ void *thr1(void *arg){
 }
 
 void *thr2(void *arg){
+	printf(" ");
 	 __VERIFIER_atomic_t3();
 	__VERIFIER_atomic_t4();
 	__VERIFIER_atomic_t5();
